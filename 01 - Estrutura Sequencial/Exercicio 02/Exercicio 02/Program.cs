3facb86faf350af97e81dfa4ca9d6badab60d54f
Lucas Oliveira <97983278+Goldenrino@users.noreply.github.com>
2024-07-10 18:29:28 -0300

    Add files via upload

---
+++ 01 - Estrutura Sequencial/Exercicio 02/Exercicio 02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio02
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            double r, a;
+
+            r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            a = Math.PI * Math.Pow(r, 2.0);
+
+            Console.WriteLine("A = " + a.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ 01 - Estrutura Sequencial/Exercicio 04/Exercicio 04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio04
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            int numero, horas;
+            double valorHora, salario;
+
+            numero = int.Parse(Console.ReadLine());
+            horas  = int.Parse(Console.ReadLine());
+            valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = horas * valorHora;
+
+            Console.WriteLine("NUMBER = " + numero);
+            Console.WriteLine("SALARY = " + salario.ToString("F2", CultureInfo.InvariantCulture));
+            
+        }
+    }
+}
