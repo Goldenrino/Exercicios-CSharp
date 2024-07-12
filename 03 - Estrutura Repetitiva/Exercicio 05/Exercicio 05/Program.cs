86cea2401f3484c1c1a87d3f936c87cd7643c02e
Lucas Oliveira <97983278+Goldenrino@users.noreply.github.com>
2024-07-12 19:49:55 -0300

    Add files via upload

---
+++ 03 - Estrutura Repetitiva/Exercicio 05/Exercicio 05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercicio05
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            int dentro = 0;
+            int fora = 0;
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) { 
+                int x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <= 20) {
+                    dentro++;
+                }
+                else {
+                    fora++;
+                }
+            }
+            Console.WriteLine(dentro + " in");
+            Console.WriteLine(fora + " out");
+        }
+    }
+}
---
+++ 03 - Estrutura Repetitiva/Exercicio 10/Exercicio 10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio10 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+
+                int primeiro = i;
+                int segundo = (int)Math.Pow(i, 2);
+                int terceiro = (int)Math.Pow(i, 3);
+                Console.WriteLine($"{primeiro} {segundo} {terceiro}");
+            }
+        }
+    }
+} 
